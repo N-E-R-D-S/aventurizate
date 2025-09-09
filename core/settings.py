798b83fb38224522c673f25dfb660ff0bb3fd3b5
deybis <damv2007@gmail.com>
2025-09-09 11:09:56 -0600

    agregadas vistas basicas para reservas y registrado en admin

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "accounts",
     "birds",
+    "reserves"
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- reserves/admin.py
+++ reserves/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Reserve, ReservePhoto
 
-# Register your models here.
+
+class ReservePhotoInline(admin.TabularInline):
+    """
+    Inline para mostrar las fotos de la reserva dentro del admin de Reserve
+    """
+    model = ReservePhoto
+    extra = 1  # Muestra un formulario extra vacío
+    readonly_fields = ('uploaded_at',)
+    fields = ('image', 'description', 'uploaded_at')
+
+
+@admin.register(Reserve)
+class ReserveAdmin(admin.ModelAdmin):
+    """
+    Admin para el modelo Reserve
+    """
+    list_display = ('name', 'get_birds', 'latitude', 'longitude')
+    search_fields = ('name', 'description')
+    list_filter = ('birds',)
+    inlines = [ReservePhotoInline]
+
+    def get_birds(self, obj):
+        """
+        Muestra las aves asociadas a la reserva como una lista
+        """
+        return ", ".join([bird.common_name for bird in obj.birds.all()])
+    get_birds.short_description = "Aves"
+
+
+@admin.register(ReservePhoto)
+class ReservePhotoAdmin(admin.ModelAdmin):
+    """
+    Admin para el modelo ReservePhoto
+    """
+    list_display = ('reserve', 'description', 'uploaded_at')
+    search_fields = ('reserve__name', 'description')
+    readonly_fields = ('uploaded_at',)
